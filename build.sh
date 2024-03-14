018c1469601ba77400d580259aebc3a97877e45d
franzgruberleitner-htl <72005833+franzgruberleitner-htl@users.noreply.github.com>
2024-03-14 15:12:02 +0100

    next try6

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 mvn -B package
 cp src/main/docker/Dockerfile target/
 
-export GITHUB_ACTOR=f.gruber-leitner@htl-leonding.ac.at
-export GITHUB_TOKEN=ZnJhbnpncnViZXJsZWl0bmVyLWh0bDpnaHBfOXdGSVQ3ZnBKNXpGeHpwOWFWd0xYNnFnUjdBOUlvM2dTYWRW
-export GITHUB_REPOSITORY=franzgruberleitner-htl
+#export GITHUB_ACTOR=f.gruber-leitner@htl-leonding.ac.at
+#export GITHUB_TOKEN=ZnJhbnpncnViZXJsZWl0bmVyLWh0bDpnaHBfOXdGSVQ3ZnBKNXpGeHpwOWFWd0xYNnFnUjdBOUlvM2dTYWRW
+#export GITHUB_REPOSITORY=franzgruberleitner-htl
 
 docker login ghcr.io -u $GITHUB_ACTOR  -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_ACTOR/backend:latest ./target
