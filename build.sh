5e6ce1f0dd261c60301df7a4982fcb11592b230e
franzgruberleitner-htl <72005833+franzgruberleitner-htl@users.noreply.github.com>
2024-03-14 11:25:22 +0100

    initial

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
+
+./env.sh
 
 mvn -B package
 cp src/main/docker/Dockerfile target/
-docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
+
+export GITHUB_ACTOR=franzgruberleitner-htl
+export GITHUB_TOKEN=ZnJhbnpncnViZXJsZWl0bmVyLWh0bDpnaHBfOXdGSVQ3ZnBKNXpGeHpwOWFWd0xYNnFnUjdBOUlvM2dTYWRW
+export GITHUB_REPOSITORY=franzgruberleitner-htl
+
+docker login ghcr.io -u $GITHUB_ACTOR  -p $GITHUB_TOKEN
 docker build --tag ghcr.io/$GITHUB_REPOSITORY/backend:latest ./target
 docker push ghcr.io/$GITHUB_REPOSITORY/backend:latest
 
---
+++ env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+export GITHUB_ACTOR=franzgruberleitner-htl
+export GITHUB_TOKEN=ZnJhbnpncnViZXJsZWl0bmVyLWh0bDpnaHBfOXdGSVQ3ZnBKNXpGeHpwOWFWd0xYNnFnUjdBOUlvM2dTYWRW
+export GITHUB_REPOSITORY=franzgruberleitner-htl
+
